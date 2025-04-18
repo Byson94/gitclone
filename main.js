dddc46ab79f64331c8ade20762f6ee624b074f22
Byson94 <byson94@outlook.com>
2025-04-18 12:37:13 +0530

    Log feature added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fs = require("node:fs"); // for accessing filesystem and changing files.
+const path = require("path");
 const { createHash } = require("crypto"); // for hashing strings.
 const zlib = require("zlib");
 
@@ -36,6 +37,9 @@ if (arguments[1]) {
       if (!fs.existsSync("./.gitclone/refs")) {
         fs.mkdirSync("./.gitclone/refs");
       }
+      if (!fs.existsSync("./.gitclone/refs/heads")) {
+        fs.mkdirSync("./.gitclone/refs/heads");
+      }
     } catch (err) {
       console.error(err);
     }
@@ -45,9 +49,11 @@ if (arguments[1]) {
       if (!fs.existsSync("./.gitclone/index")) {
         fs.writeFileSync("./.gitclone/index", "");
       }
-
       if (!fs.existsSync("./.gitclone/HEAD")) {
         fs.writeFileSync("./.gitclone/HEAD", process.cwd()); // get current active dir
+      }
+      if (!fs.existsSync("./.gitclone/refs/heads/master")) {
+        fs.writeFileSync("./.gitclone/refs/heads/master", ""); // get current active dir
       }
     } catch (err) {
       console.error(err);
@@ -77,12 +83,17 @@ if (arguments[1]) {
             console.error("Compression error:", err);
             return;
           }
-          if (!fs.existsSync(`./.gitclone/objects/${dataHash}`)) {
-            fs.writeFileSync(
-              "./.gitclone/objects/" + dataHash,
-              compressedData,
-              "utf8"
-            );
+          const dirPath = path.join(
+            ".gitclone",
+            "objects",
+            ...dataHash.split("/").slice(0, -1)
+          );
+          const filePath = path.join(dirPath, dataHash.split("/").pop());
+          if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+          }
+          if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, compressedData, "utf8");
           }
         });
         // console.log(dataHash);
@@ -116,10 +127,85 @@ if (arguments[1]) {
     }
   } else if (arguments[1] === "commit") {
     if (arguments[2] === "-m") {
-      console.log("commits the changes");
       if (arguments[3]) {
+        // we will read the index and copy it directly to the tree
+        let indexData = fs
+          .readFileSync("./.gitclone/index", "utf8")
+          .split("\n")
+          .slice(1); // slice because the 1st line will be a \n
+        let parentName = fs.readFileSync(
+          "./.gitclone/refs/heads/master",
+          "utf8"
+        );
+        let treeData = "";
+
+        indexData = indexData.map((line) => {
+          treeData += `${line} \n`;
+        });
+
+        if (parentName === "") {
+          treeData += `[parent] > none \n`;
+        } else {
+          treeData += `[parent] > ${parentName} \n`;
+        } // write the parent hash
+
+        treeData += `[message] > ${arguments[3]}`;
+
+        const treeHash = createHash("sha256").update(treeData).digest("base64");
+
+        const dirPath = path.join(
+          ".gitclone",
+          "objects",
+          ...treeHash.split("/").slice(0, -1)
+        );
+        const filePath = path.join(dirPath, treeHash.split("/").pop());
+        if (!fs.existsSync(dirPath)) {
+          fs.mkdirSync(dirPath, { recursive: true });
+        }
+        if (!fs.existsSync(filePath)) {
+          fs.writeFileSync(filePath, treeData, "utf8");
+        }
+        fs.writeFileSync(".gitclone/refs/heads/master", treeHash, "utf8");
+        fs.writeFileSync(".gitclone/index", "", "utf8");
+
+        // console.log(indexData);
       } else {
         console.error("ERROR: Invalid argument found");
+      }
+    }
+  } else if (arguments[1] === "log") {
+    let latestCommit = fs.readFileSync("./.gitclone/refs/heads/master", "utf8");
+
+    if (latestCommit === "") {
+      console.error("ERROR: No commits found");
+    } else {
+      let currentCommit = latestCommit;
+
+      while (currentCommit !== "none") {
+        let commitData = fs.readFileSync(
+          `${process.cwd()}/.gitclone/objects/${currentCommit}`,
+          "utf8"
+        );
+
+        let lines = commitData.split("\n");
+
+        console.log(`Commit: ${currentCommit}`);
+        console.log(
+          lines.find((line) => line.startsWith("[message] > ")) + "\n"
+        );
+
+        // continue the loop
+        let parentLine = lines.find((line) => line.startsWith("[parent] > "));
+        let parentHash = parentLine
+          ? parentLine.split(" > ")[1].trim()
+          : "none";
+
+        if (parentHash === "none") {
+          console.log("End of commit history.");
+          break;
+        }
+
+        currentCommit = parentHash;
       }
     }
   }
